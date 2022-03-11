ffa5660cb6961b82634dbccf6718e4f37f510707
root <faythkoko@gmail.com>
2022-03-11 14:54:07 -0800

    Update 3-print alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,11 @@
+/*
+ * File: 3-print_alphabets.c
+ * Auth:Chepkorir Faith
+ */
 #include <stdio.h>
 
 /**
- * Main - Entry point
+ * Main - Prints the alphabet in lowercase, and then in uppercase.
  *
  * Return: Always 0 (Success)
  */
@@ -12,7 +16,6 @@ int main(void)
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
-
 	/* print uppercase letters */
 	for (ch = 'A'; ch <= 'Z'; ch++)
 		putchar(ch);
