c469d46a7f9c5239921f0b8adc9b5205d7d27160
root <faythkoko@gmail.com>
2022-03-12 13:36:06 -0800

    lowercase alphabet except q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,3 +1,7 @@
+/*
+ * File: 4-print_alphabt.c
+ * Auth:Chepkorir Faith
+ */
 #include <stdio.h>
 
 /**
@@ -5,14 +9,16 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	char ch;
 
-	for (ch = 'a'; ch <= 'z' && ch != 'q' && ch != 'e'; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
+		if ( ch != 'q' && ch != 'e')
+		{
 		putchar(ch);
+		}
 	}
 	return (0);
 }
