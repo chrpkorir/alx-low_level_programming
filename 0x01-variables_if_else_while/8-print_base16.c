3a56a2bc4b3954a10bb4c6f4307cf1b37f00ce14
root <faythkoko@gmail.com>
2022-03-14 00:27:46 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main- Print Hexadecimal
+ *
+ * Return: Always 0(Successful)
+ */
+int main(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	for (n = 'a'; n <= 'f'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
