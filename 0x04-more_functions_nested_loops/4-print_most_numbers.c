21137ae7e93a1803f9ba4f455a573c3727755232
root <faythkoko@gmail.com>
2022-03-19 05:10:44 -0700

    Update Print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers -  checks for checks for a digit (0 through 9)
- *   
+ * print_most_numbers - checks for checks for a digit (0 through 9)   
  * Return: Always 0.
  */
 void print_most_numbers(void)
