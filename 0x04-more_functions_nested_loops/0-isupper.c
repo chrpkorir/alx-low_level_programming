ebec58f58bae2db49cf316513614a88dd68cb31a
root <faythkoko@gmail.com>
2022-03-19 05:00:47 -0700

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,15 +7,17 @@
  * Return: 1 if c isuppercase
  * Return: 0 otherwise
  */
-
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
+
+
+	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
 	else
 	{
-	return (0);
+		return (0);
 	}
+
 }
