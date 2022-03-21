37f3698e752a70eb58eae700367e838576164180
chrpkorir <chrpkorir>
2022-03-21 13:33:00 +0000

    Prints a string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _puts - a function that prints a string,
- * followed by new line, to stdout
+ * _puts - prints a string followed by new line, to stdout
+ * @str: string to print
  */
 void _puts(char *str)
 {
-	while (*str != '0')
+	while (*str != '\0')
 	{
 		_putchar(*str++);
 	}
