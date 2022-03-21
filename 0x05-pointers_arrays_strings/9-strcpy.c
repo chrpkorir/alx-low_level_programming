4ad3566faee8495354e08003f7bef9bbfab91a85
chrpkorir <chrpkorir>
2022-03-21 20:39:49 +0000

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -4,6 +4,8 @@
  * *_strcpy - copies the string pointed to by src,
  * including the terminating null byte(\0)'
  * to the buffer pointed to by dest
+ * @dest: copy to
+ * @str: copy from
  * Return: pointer to dest
  */
 char *_strcpy(char *dest, char *src)
