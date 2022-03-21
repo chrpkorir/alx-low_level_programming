28062dbb1617f66a24b85054d7c04ac985a5d366
chrpkorir <chrpkorir>
2022-03-21 13:42:08 +0000

    Print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by new line
+ * @s: string to print
+ */
+void print_rev(char *s)
+{
+	char *s;
+
+	while (*s !='\0')
+	{
+		_putchar(*s--);
+	}
+	_putchar('\n');
+}
