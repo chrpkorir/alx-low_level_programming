b65b5f16b92e2cca007c8d49f5d42eb2c67d7cd4
chrpkorir <chrpkorir>
2022-03-22 12:30:49 +0000

    Update 3-islower

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,13 +3,14 @@
 /**
  * _isalpha - checks for alphabetic character
  * @c: character to be checked
- * Return: 1 if c is a character, 0 otherwise 
+ * Return: 1 if c is a letter, 0 otherwise 
  */
 int _isalpha(int c)
 {
 	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90)) 
 	{
 		return (1);
-	}else
+	}
+	else
 		return (0);
 }
