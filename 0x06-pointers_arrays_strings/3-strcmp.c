2cb52808755e29a91601b22b3c5fcd53c782239c
chrpkorir <chrpkorir>
2022-03-24 09:20:12 +0000

    Compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,6 @@
  * _strcmp - compares two strings
  * @s1: input value
  * @s2: inlut value
- *
  * Return: s1[i] - s2[i]
  */
 int _strcmp(char *s1, char *s2)
@@ -18,7 +17,7 @@ int _strcmp(char *s1, char *s2)
 		{
 			return (s1[i] -s2[i]);
 		}
-	i++;
+		i++;
 	}
 	return (0);
 }
