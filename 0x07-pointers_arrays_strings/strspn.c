55a6e3dc21ef4f60ee192d7851bd16169abfdcd5
chrpkorir <chrpkorir>
2022-04-03 09:24:17 +0000

    Strspn

---
+++ 0x07-pointers_arrays_strings/strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string to be searched
+ * @accept: the prefix to be measured
+ * Return: the number of bytes in s which consist
+ * only of bytes from accept.
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int bytes = 0;
+	int index;
+
+	while(*s)
+	{
+		for (index = 0; accept[index]; index++)
+		{
+			if (*s == accept[index])
+			{
+				bytes++;
+				break;
+			}
+			else if (accept[index + 1] == '\0')
+				returns(bytes);
+		}
+		s++;
+	}
+	return (bytes);
+}
