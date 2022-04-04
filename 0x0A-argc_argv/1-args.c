c72025095df842c52c81051e80e07fd0ea30b790
chrpkorir <chrpkorir>
2022-04-04 09:36:29 +0000

    Prints the number of arguments passed onto it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.c"
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed to it
+ * @argc: store number of arguments 
+ * @argv: store array of strings
+ *
+ * Return:Always 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
