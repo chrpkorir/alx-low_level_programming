9996e8d23212c8a98d9a7b8c08c33936319f6bd2
chrpkorir <faythkoko@gmail.com>
2022-04-08 23:39:41 +0000

    Diagonal sum

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -5,7 +5,7 @@
 
 /**
  * print_diagsums - Prints the sum of the two diagonals
- *                  of a square matrix of integers.
+ * of a square matrix of integers.
  * @a: The matrix of integers.
  * @size: The size of the matrix.
  */
