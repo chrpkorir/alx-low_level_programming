9def91f25b8f9b181efe0b047f0093f7f762561e
chrpkorir <faythkoko@gmail.com>
2022-04-12 10:49:24 +0000

    Unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,7 +11,7 @@ int main(void)
 	long l = 59;
 	long fd = 1;
 	long syscall = 1;
-	long ret = 0;__asm__("syscall" : "=a" (ret) : "a" (syscall),
+	long ret = 0; __asm__("syscall" : "=a" (ret) : "a" (syscall),
 	"D" (fd),
 	"S" (s),
 	"d" (l));
