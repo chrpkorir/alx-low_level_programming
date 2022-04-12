6ad1c32b6abfb0ced1b80db0867e647402d3d2ec
chrpkorir <faythkoko@gmail.com>
2022-04-12 08:04:14 +0000

    A program to print out the file it was compiled from

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,11 @@
-#include <stdlib.>
 #include <stdio.h>
 
 /**
  * main - print the file name compiled form
  *
+ * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
 	printf("%s\n", __FILE__);
 
