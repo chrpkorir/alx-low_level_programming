e15e3371a30d5bb4f49e2ff5b7c0caa6abf80d9c
chrpkorir <faythkoko@gmail.com>
2022-04-12 10:47:04 +0000

    Unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,14 +5,13 @@
  * Return: 1
  */
 
-int main (void)
+int main(void)
 {
 	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	long l = 50;
+	long l = 59;
 	long fd = 1;
 	long syscall = 1;
-	long ret = 0;
-	__asm__("syscall" : "=a" (ret) : "a" (syscall),
+	long ret = 0;__asm__("syscall" : "=a" (ret) : "a" (syscall),
 	"D" (fd),
 	"S" (s),
 	"d" (l));
