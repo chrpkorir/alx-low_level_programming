e90d37de46044a9c257b8777288ccd1a3755bc14
chrpkorir <faythkoko@gmail.com>
2022-04-12 10:56:57 +0000

    Update

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,14 +3,12 @@
 /**
  * _isalpha - checks for alphabetic character
  * @c: character to be checked
- * Return: 1 if c is a letter, 0 otherwise 
+ * Return: 1 if c is a letter, 0 otherwise
  */
 int _isalpha(int c)
 {
 	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90)) 
-	{
 		return (1);
-	}
 	else
 		return (0);
 }
