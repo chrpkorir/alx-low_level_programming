d70dd1ac66c22214dd67ee0daaedf60b089e8bb5
chrpkorir <faythkoko@gmail.com>
2022-05-03 22:51:34 +0000

    Creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,12 +9,10 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int w;
-	int len = 0;
+	int fd, int w, int len = 0;
 
 	if (filename == NULL)
-		return(-1);
+		return (-1);
 
 	if (text_content != NULL)
 	{
